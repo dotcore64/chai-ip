bf8dfcd2c14496368b6376d6d229fe5a5c76449f
Julian Grinblat <julian@dotcore.co.il>
2024-10-01 19:28:04 +0900

    fix

--- types/test.ts
+++ types/test.ts
@@ -1,8 +1,8 @@
-import chai from 'chai';
+import { use, expect } from 'chai';
 import ChaiIp from 'chai-ip'; // eslint-disable-line import/no-unresolved,node/no-missing-import
 
-chai.use(ChaiIp);
+use(ChaiIp);
 
-chai.expect('127.0.0.1').to.be.an.ip; // eslint-disable-line no-unused-expressions
-chai.expect('127.0.0.1').to.be.an.ipv4; // eslint-disable-line no-unused-expressions
-chai.expect('127.0.0.1').to.be.an.ipv6; // eslint-disable-line no-unused-expressions
+expect('127.0.0.1').to.be.an.ip; // eslint-disable-line no-unused-expressions
+expect('127.0.0.1').to.be.an.ipv4; // eslint-disable-line no-unused-expressions
+expect('127.0.0.1').to.be.an.ipv6; // eslint-disable-line no-unused-expressions
